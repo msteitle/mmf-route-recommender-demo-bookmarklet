3b444b8d3213fb2996a9b1acc1627082fddd8cb8
Matt Steitle <matt.steitle@mapmyfitness.com>
2013-03-07 15:34:29 -0700

    Adding pause button, hopefully improved managing of difficult route generate attempts.

--- demo.js
+++ demo.js
@@ -1,11 +1,24 @@
 (function () {
 window.gen_complete = true;
+var process_count = 0; // start with 1 because initial load publishes an additional end.processing topic
 
 // PubSub already in require directory - just request ref
 window.require(['pubsub'], function (p) {
     p.subscribe('end.processing', function () {
         console.log('Finished');
-        window.gen_complete = true;
+        process_count--;
+
+        if (process_count === 0) {
+            window.gen_complete = true;
+        }
+    });
+});
+
+window.require(['pubsub'], function (p) {
+    p.subscribe('start.processing', function () {
+        console.log('Starting');
+        process_count++;
+        window.gen_complete = false;
     });
 });
 
@@ -16,7 +29,7 @@ var locations = [
     'Vancouver, BC',
     'San Francisco, CA',
     'Rome',
-    //'Seattle, WA', // Seattle is trouble
+    'Seattle, WA', // Seattle is trouble
     'Osaka-jo, Osaka, Japan',
     'Reflecting Pool, Washington DC',
     'Amsterdam',
@@ -41,10 +54,37 @@ d_i = 0,
 interval = 11,
 interval_id,
 countdown = 1,
-$time = $('<span>')
+$time = $('<span id="demo_countdown">')
     .text('0')
     .css('font-size', '120%'),
-demo = function () {
+$message = $('<p id="demo_message">')
+    .css({
+    "text-align": "center",
+    "clear": "both",
+    "margin-top": "2rem",
+    "font-weight": "bold",
+    "border": "1px solid #666",
+    "padding": "0.5rem",
+    "border-width": "1px 0",
+    "background-color": "#ddd"
+    })
+    .append(
+        'Generating new route in ',
+        $time,
+        ' seconds.'
+    ),
+$pause = $('<a id="demo_pause" data-active="Pause" data-inactive="Unpause" class="button">Pause</a>')
+    .on('click', function () {
+        if (interval_id) {
+            undemo();
+            $pause.text($pause.data('inactive'));
+        }
+        else {
+            demo();
+            $pause.text($pause.data('active'));
+        }
+    }),
+all_the_things = function () {
     if (!window.gen_complete) {
         return;
     }
@@ -54,9 +94,8 @@ demo = function () {
     }
     $search.val(locations[s_i]).trigger('change');
     $distance.val(distances[Math.floor(Math.random() * d_len)]);
-    window.gen_complete = false;
     $generate.click();
-    console.log('Started', locations[s_i]);
+    //console.log('Started', locations[s_i]);
 
     // set up next iteration
     s_i = s_i < s_len - 1 ? s_i + 1 : 0;
@@ -64,27 +103,16 @@ demo = function () {
 },
 undemo = function () {
     window.clearInterval(interval_id);
+    interval_id = null;
+},
+demo = function () {
+    interval_id = window.setInterval(all_the_things, 1000);
 };
-$('#map_container').find('div').filter('[title="MapQuest OSM layer"]').click();
-$('#left').find('.inner').append(
-    $('<p>')
-        .css({
-        "text-align": "center",
-        "clear": "both",
-        "margin-top": "2rem",
-        "font-weight": "bold",
-        "border": "1px solid #666",
-        "padding": "0.5rem",
-        "border-width": "1px 0",
-        "background-color": "#ddd"
-        })
-        .append(
-            'Generating new route in ',
-            $time,
-            ' seconds.'
-        )
-);
 $('label[for=route_length]').text('Length (approx.):');
-interval_id = window.setInterval(demo, 1000);
+$('#map_container').find('div').filter('[title="MapQuest OSM layer"]').click();
+$('#left').find('.inner').append($message);
 window.undemo = undemo;
+
+demo();
+$message.append('<br/><br/>', $pause);
 }());
